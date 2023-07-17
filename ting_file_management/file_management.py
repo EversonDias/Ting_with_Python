f17e9084d54464af162956db8d5aa37f02ca92a0
everson <eversond14s@gmail.com>
2023-07-17 17:02:26 -0300

    created method file process

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -16,3 +16,20 @@ def txt_importer(path_file):
                 return data
         else:
             print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+
+
+def file_process(path_file, list):
+    file_name, file_ext = os.path.splitext(path_file)
+    name = file_name.split('/')[-1]
+    file = txt_importer(path_file)
+    isRepeated = False
+    for index in range(len(list)):
+        if name in list.search(index)["nome_do_arquivo"]:
+            isRepeated = True
+    data = {}
+    if not isRepeated:
+        data["nome_do_arquivo"] = name
+        data["qtd_linhas"] = len(file)
+        data["linhas_do_arquivo"] = file
+        list.enqueue(data)
+        print(data, file=sys.stderr)
