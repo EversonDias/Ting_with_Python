2731e2c81e322caeca1e01a99781d9f77559ffcb
everson <eversond14s@gmail.com>
2023-07-17 16:15:57 -0300

    created method text_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,18 @@
+import os
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    file_name, file_ext = os.path.splitext(path_file)
+    if file_ext != ".txt":
+        print("Formato inválido", file=sys.stderr)
+    else:
+        if os.path.exists(path_file):
+            with open(path_file, 'r') as file:
+                lines = file.readlines()
+                data = []
+                for line in lines:
+                    data.append(line.replace('\n', ''))
+                return data
+        else:
+            print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
