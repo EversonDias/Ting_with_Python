dfbdc1d426531e9fc06519c183a79ab1c87c2a90
everson <eversond14s@gmail.com>
2023-07-17 22:01:04 -0300

    create tests priority_queue

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,21 @@
-# from ting_file_management.priority_queue import PriorityQueue
+import pytest
+from ting_file_management.priority_queue import PriorityQueue
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    project = PriorityQueue()
+    large_path = {"nome": "large_file", "qtd_linhas": 20}
+    small_path = {"nome": "small_file", "qtd_linhas": 3}
+
+    project.enqueue(large_path)
+    assert len(project) == 1
+
+    project.enqueue(small_path)
+    assert project.search(0) == small_path
+    assert project.search(1) == large_path
+    
+    project.dequeue()
+    assert len(project) == 1
+
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        project.search(2)
